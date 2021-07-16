66f5f90bd5dafbf579c31d365c64c70899f90ae3
An Ho <anho@devpanel.com>
2021-07-16 17:29:29 +0000

    Update re-config.sh

--- .devpanel/re-config.sh
+++ .devpanel/re-config.sh
@@ -24,8 +24,16 @@ STATIC_FILES_PATH="$WEB_ROOT/files/"
 SETTINGS_FILES_PATH="$WEB_ROOT/settings.php"
 DEFAULT_SETTINGS_FILE="https://raw.githubusercontent.com/backdrop/backdrop/1.19.1/settings.php"
 
+#== Add Backdrop Drush extension
+mkdir -p  ~/.drush/commands && cd ~/.drush/commands
+rm backdrop -fR
+curl -sLo backdrop-drush.zip https://github.com/backdrop-contrib/backdrop-drush-extension/archive/1.x-1.x.zip
+unzip -qo backdrop-drush.zip -d backdrop && rm backdrop-drush.zip
+drush cc drush
+
 #== Extract static files
 if [[ -f "$APP_ROOT/.devpanel/dumps/files.tgz" ]]; then
+  mkdir -p $STATIC_FILES_PATH
   tar xzf "$APP_ROOT/.devpanel/dumps/files.tgz" -C $STATIC_FILES_PATH
 fi
 
@@ -43,6 +51,9 @@ if [[ -f "$SETTINGS_FILES_PATH" ]]; then
 fi
 curl -o $SETTINGS_FILES_PATH $DEFAULT_SETTINGS_FILE --silent
 sed -i -e "s/^\$database =.*/\$database = 'mysql:\/\/$DB_USER:$DB_PASSWORD@$DB_HOST\/$DB_NAME';/g" $SETTINGS_FILES_PATH
+
+#== Rename config directory
+sudo mv $STATIC_FILES_PATH/$(ls $STATIC_FILES_PATH -a | grep "config_") $STATIC_FILES_PATH/$(echo 'echo "config_" . md5($database);' | cat $SETTINGS_FILES_PATH - | php)
 
 #== Update permission
 sudo find $APP_ROOT -type f -exec chmod 664 {} \;
