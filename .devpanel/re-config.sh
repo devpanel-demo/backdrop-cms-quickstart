94f9b30b45e9cdb74a8211063546f7e0a81b64c6
kha <kha@devpanel.com>
2023-05-10 16:56:00 +0700

    Update devpanel config

--- .devpanel/re-config.sh
+++ .devpanel/re-config.sh
@@ -22,23 +22,12 @@ fi
 
 STATIC_FILES_PATH="$WEB_ROOT/files"
 SETTINGS_FILES_PATH="$WEB_ROOT/settings.php"
-DEFAULT_SETTINGS_FILE="https://raw.githubusercontent.com/backdrop/backdrop/1.24.1/settings.php"
-CONFIG_DIR="";
-
-#== Create settings files
-echo 'Create database settings ...';
-if [[ ! -f "$SETTINGS_FILES_PATH" ]]; then
-  curl -o $SETTINGS_FILES_PATH $DEFAULT_SETTINGS_FILE --silent
-fi
-sed -i -e "s/^\$database =.*/\$database = 'mysql:\/\/'.getenv\('DB_USER'\).':'.getenv\('DB_PASSWORD'\).'@'.getenv\('DB_HOST'\).'\/'.getenv\('DB_NAME'\);/g" $SETTINGS_FILES_PATH
-
-
 
 #== Extract static files
 if [[ $(drush status bootstrap) == '' ]]; then
   if [[ -f "$APP_ROOT/.devpanel/dumps/files.tgz" ]]; then
     echo  'Extract static files ...'
-    mkdir -p $STATIC_FILES_PATH
+    sudo mkdir -p $STATIC_FILES_PATH
     sudo tar xzf "$APP_ROOT/.devpanel/dumps/files.tgz" -C $STATIC_FILES_PATH
   fi
 
@@ -52,20 +41,30 @@ if [[ $(drush status bootstrap) == '' ]]; then
   fi
 fi
 
+#== Create settings file
+if [[ ! -f "$SETTINGS_FILES_PATH" ]]; then
+  echo "Create settings file ..."
+  sudo cp $APP_ROOT/.devpanel/backdrop-cms-settings.php $SETTINGS_FILES_PATH
+fi
+
 
 #== Point config directory
-if [[ -d $(find $APP_ROOT -maxdepth 1 -type d -name  'config_*' |  head -n 1) ]];  then
-  CONFIG_DIR=$(find $APP_ROOT -maxdepth 1 -type d -name  'config_*'| head -n 1 | sed 's/\//\\\//g')
-fi
+echo "Point config directory ..."
 if [[ -d $(find $STATIC_FILES_PATH -maxdepth 1 -type d -name  'config_*' |  head -n 1) ]];  then
   CONFIG_DIR=$(find $STATIC_FILES_PATH -maxdepth 1 -type d -name  'config_*'| head -n 1 | sed 's/\//\\\//g')
 fi
+
+if [[ -d $(find $APP_ROOT -maxdepth 1 -type d -name  'config_*' |  head -n 1) ]];  then
+  CONFIG_DIR=$(find $APP_ROOT -maxdepth 1 -type d -name  'config_*'| head -n 1 | sed 's/\//\\\//g')
+fi
+
 if [[ $CONFIG_DIR != '' ]]; then
   echo 'Create config settings ...';
   sed -i -e "s/^\$config_directories\['active'\].*/\$config_directories\['active'\] = '$CONFIG_DIR\/active';/g" $SETTINGS_FILES_PATH
   sed -i -e "s/^\$config_directories\['staging'\].*/\$config_directories\['staging'\] = '$CONFIG_DIR\/staging';/g" $SETTINGS_FILES_PATH
 fi
 
-#== Update permission
-sudo chown -R www:www-data $APP_ROOT
+echo "Update permission ..."
 sudo chown -R www-data:www-data $STATIC_FILES_PATH
+sudo chown www:www-data $SETTINGS_FILES_PATH
+sudo chmod 664 $SETTINGS_FILES_PATH
